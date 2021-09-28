4e366e3a6ea43c5d32571737d2569eed013ac378
beresandras <30826103+beresandras@users.noreply.github.com>
2021-09-28 09:29:56 +0200

    fid added

--- metrics.py
+++ metrics.py
@@ -57,3 +57,58 @@ class KID(keras.metrics.Metric):
 
     def reset_states(self):
         self.kid_tracker.reset_states()
+
+
+# class FID(keras.metrics.Metric):
+#     def __init__(self, name='fid', **kwargs):
+#         super().__init__(name=name, **kwargs)
+
+#         self.encoder = keras.Sequential([
+#             layers.InputLayer(input_shape=(32, 32, 3)),
+#             preprocessing.Rescaling(255),
+#             preprocessing.Resizing(height=75, width=75),
+#             layers.Lambda(keras.applications.inception_v3.preprocess_input),
+#             keras.applications.InceptionV3(include_top=False, input_shape=(75, 75, 3), weights="imagenet"),
+#             layers.GlobalAveragePooling2D(),
+#         ])
+
+#         self.bank_size = 5794 // batch_size * batch_size
+#         self.feature_dimensions = self.encoder.output_shape[1]
+
+#         self.real_feature_bank = tf.Variable(
+#             tf.zeros(shape=(self.bank_size, self.feature_dimensions)),
+#             trainable=False,
+#         )
+#         self.generated_feature_bank = tf.Variable(
+#             tf.zeros(shape=(self.bank_size, self.feature_dimensions)),
+#             trainable=False,
+#         )
+#         self.bank_index = tf.Variable(0)
+
+#     def update_state(self, real_images, generated_images, sample_weight=None):
+#         real_features = self.encoder(real_images, training=False)
+#         generated_features = self.encoder(generated_images, training=False)
+
+#         self.real_feature_bank[self.bank_index : self.bank_index + batch_size].assign(real_features)
+#         self.generated_feature_bank[self.bank_index : self.bank_index + batch_size].assign(generated_features)
+#         self.bank_index.assign_add(batch_size)
+
+#     def result(self):
+#         if self.bank_index < self.bank_size:
+#             return 0.0
+#         else:
+#             real_mean = tf.math.reduce_mean(self.real_feature_bank, axis=0)
+#             generated_mean = tf.math.reduce_mean(self.generated_feature_bank, axis=0)
+
+#             real_covariance = tf.transpose(self.real_feature_bank) @ self.real_feature_bank / self.bank_size - tf.reduce_sum(real_mean ** 2)
+#             generated_covariance = tf.transpose(self.generated_feature_bank) @ self.generated_feature_bank / self.bank_size - tf.reduce_sum(generated_mean ** 2)
+
+#             tf.print(tf.shape(real_covariance), tf.shape(generated_covariance))
+
+#             fid = tf.reduce_sum((real_mean - generated_mean) ** 2) + tf.linalg.trace(tf.linalg.sqrtm(real_covariance @ tf.transpose(generated_covariance)))
+#             return fid
+
+#     def reset_state(self):
+#         self.real_feature_bank.assign(tf.zeros(shape=(self.bank_size, self.feature_dimensions)))
+#         self.generated_feature_bank.assign(tf.zeros(shape=(self.bank_size, self.feature_dimensions)))
+#         self.bank_index.assign(0)
